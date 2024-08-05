6f6babb0d6a21afb638d5743b10f58e28401a636
FATEC | João Victor Soares Dias <fatec.jdias@icolabora.com.br>
2024-08-04 23:13:42 -0300

    Adicionando o Material UI (MUI) e estilizando o header e a home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,21 @@
 import "./header.css";
+
 import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 function Header() {
-    return(
-        <header>
-            <Link className="logo" to="/">React Movies</Link>
-            <Link className="my-movies" to="/my-movies">Meus filmes</Link>
-        </header>
-    );
+  return (
+    <header>
+      <Link className="logo" to="/">
+        React Movies
+      </Link>
+
+      <Button variant="outlined" component={RouterLink} to="/my-movies">
+        Meus Filmes
+      </Button>
+    </header>
+  );
 }
 
 export default Header;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,56 +1,80 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import "./home.css";
 
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
 function Home() {
-    // Criando um estado para armazenar filmes
-    const [movies, setMovies] = useState([]);
+  // Criando um estado para armazenar filmes
+  const [movies, setMovies] = useState([]);
 
-    // Criando um estado para loading
-    const [loading, setLoading] = useState(true);
+  // Criando um estado para loading
+  const [loading, setLoading] = useState(true);
 
-    // Criando um useEffect para carregar os filmes assim que a aplicação abrir
-    useEffect(() => {
-        async function loadMovies(){
-            const response = await api.get("movie/now_playing", {
-                params: {
-                    api_key: "587d74cd16d8202b96fbd8ee84d49a1f",
-                    language: "pt-BR",
-                    page: 1
-                }
-            });
+  // Criando um useEffect para carregar os filmes assim que a aplicação abrir
+  useEffect(() => {
+    async function loadMovies() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "587d74cd16d8202b96fbd8ee84d49a1f",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
 
-            // Carregando os filmes no array
-            setMovies(response.data.results.slice(0, 10));
-            setLoading(false);
-        }
-
-        loadMovies();
-    }, []);
-
-    if(loading) {
-        return(
-            <div className="loading">
-                <h2>Carregando filmes...</h2>
-            </div>
-        );
+      // Carregando os filmes no array
+      setMovies(response.data.results.slice(0, 10));
+      setLoading(false);
     }
-    return(
-        <div className="container">
-            <div className="movie-list">
-                {movies.map((movie) => {
-                    return(
-                        <article key={movie.id}>
-                            <strong>{movie.title}</strong>
-                            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
-                            <Link to={`/movie/${movie.id}`}>Acessar</Link>
-                        </article>
-                    )
-                })}
-            </div>
-        </div>
+
+    loadMovies();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
     );
+  }
+  return (
+    <div className="container">
+      <Card sx={{ maxWidth: 345 }}>
+        {movies.map((movie) => {
+          return (
+            <>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                title={movie.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {movie.title}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  component={RouterLink}
+                  to={`/movie/${movie.id}`}
+                >
+                  Acessar
+                </Button>
+              </CardActions>
+            </>
+          );
+        })}
+      </Card>
+    </div>
+  );
 }
 
 export default Home;
